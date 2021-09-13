6c7e821f2c559c2e57a395a565ab1c916d639444
YuOkamoto <yuokamoto1988@gmail.com>
2021-09-14 00:25:14 +0900

    Move turtlebot from turtlebotUE

--- Source/UE_rapyuta_assets/UE_rapyuta_assets.Build.cs
+++ Source/UE_rapyuta_assets/UE_rapyuta_assets.Build.cs
@@ -6,34 +6,11 @@ public class UE_rapyuta_assets : ModuleRules
 {
 	public UE_rapyuta_assets(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				"rclUE"
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "rclUE" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
 			
 
 	}
