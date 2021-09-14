60104df06f3f1cfb5696ab092b40e84c1382e914
duc <ducanh.than@rapyuta-robotics.com>
2021-09-14 14:49:28 +0900

    [UE_rapyuta_assets.Build.cs] Add PhysicsCore dependency

--- Source/RapyutaSimulationPlugins/RapyutaSimulationPlugins.Build.cs
+++ Source/RapyutaSimulationPlugins/RapyutaSimulationPlugins.Build.cs
@@ -8,7 +8,7 @@ public class RapyutaSimulationPlugins : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "rclUE" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "rclUE", "PhysicsCore" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { });
             
