5388d92296a50859511ccb97a3a1e687cc69c31e
duc <ducanh.than@rapyuta-robotics.com>
2022-08-05 17:06:13 +0900

    update cmt

--- Source/RapyutaSimulationPlugins/Public/Core/RRCrowdAIController.h
+++ Source/RapyutaSimulationPlugins/Public/Core/RRCrowdAIController.h
@@ -11,7 +11,7 @@
 #include "RRCrowdAIController.generated.h"
 
 /**
- * @brief Base Crowd AI Controller for Character-based classes, utilizing UCrowdFollowingComponent
+ * @brief Base Crowd AI Controller for Pawn-based classes, utilizing UCrowdFollowingComponent
  *
  * @sa [AAIController](https://docs.unrealengine.com/4.27/en-US/API/Runtime/AIModule/AAIController/)
  * @sa https://answers.unrealengine.com/questions/871116/view.html
