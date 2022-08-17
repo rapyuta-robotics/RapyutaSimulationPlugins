1aa2b8f3ed64ebc366f64535757fc11a32a4c63f
YuOkamoto <yuokamoto1988@gmail.com>
2022-08-17 14:54:11 +0900

    fix script error

--- ExternalTest/rr_sim_tests/rr_sim_tests/test_random_spawn.py
+++ ExternalTest/rr_sim_tests/rr_sim_tests/test_random_spawn.py
@@ -50,7 +50,7 @@ def main(args=None):
         return
 
     is_robot_spawned, _ = wait_for_spawned_entity(robot_name, 10.0)
-    if is_robot_spawned
+    if is_robot_spawned:
         print(robot_name + 'is already exists.')
         return
 
