527e8c11ac6849bf8b8db4258ce4151b120ad2d9
tetorea <tetorea@github.com>
2022-08-23 17:28:33 +0900

    fix formatting, add comment

--- Source/RapyutaSimulationPlugins/Private/Tools/SimulationState.cpp
+++ Source/RapyutaSimulationPlugins/Private/Tools/SimulationState.cpp
@@ -223,22 +223,29 @@ void ASimulationState::AttachSrv(UROS2GenericSrv* Service)
             Entity2->AttachToActor(Entity1, FAttachmentTransformRules::KeepWorldTransform);
 
             // disable collision check with attached actor (Entity2) when Entity1 moves
-            for( auto component : Entity1->GetComponents() )
+            for (auto component : Entity1->GetComponents())
             {
-                auto primitiveComp = Cast<UPrimitiveComponent>( component );
-                if ( !primitiveComp ) continue;
-                primitiveComp->IgnoreActorWhenMoving( Entity2, true );
+                auto primitiveComp = Cast<UPrimitiveComponent>(component);
+                if (!primitiveComp)
+                {
+                    continue;
+                }
+                primitiveComp->IgnoreActorWhenMoving(Entity2, true);
             }
         }
         else
         {
             Entity2->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
 
-            for( auto component : Entity1->GetComponents() )
+            // enable collisions between the 2 actors when Entity1 moves
+            for (auto component : Entity1->GetComponents())
             {
-                auto primitiveComp = Cast<UPrimitiveComponent>( component );
-                if ( !primitiveComp ) continue;
-                primitiveComp->IgnoreActorWhenMoving( Entity2, false );
+                auto primitiveComp = Cast<UPrimitiveComponent>(component);
+                if (!primitiveComp)
+                {
+                    continue;
+                }
+                primitiveComp->IgnoreActorWhenMoving(Entity2, false);
             }
         }
     }
