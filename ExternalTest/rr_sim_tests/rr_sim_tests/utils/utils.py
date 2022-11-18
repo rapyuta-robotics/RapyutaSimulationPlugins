e6b774f8205e9d84eef0e4e5f6f9d5a1aed16a2d
yuokamoto <yuokamoto1988@gmail.com>
2022-11-18 15:38:02 +0900

    make cmd_vel publishing more reliable in test and apply flake8 by pre… (#114)
    * make cmd_vel publishing more reliable in test and apply flake8 by pre-commit
    
    * change max line length to 88

--- ExternalTest/rr_sim_tests/rr_sim_tests/utils/utils.py
+++ ExternalTest/rr_sim_tests/rr_sim_tests/utils/utils.py
@@ -1,35 +1,46 @@
 #! /usr/bin/env python3
 # Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
 
-import os
 import asyncio
 import time
 
 # rclpy
 import rclpy
-from rclpy.node import Node
-from rclpy.qos import QoSProfile, QoSDurabilityPolicy
-from rclpy.executors import SingleThreadedExecutor
 
 # other ros
 from geometry_msgs.msg import Twist
-
-# UE_msgs
-from ue_msgs.msg import EntityState
-from ue_msgs.srv import SpawnEntity
+from rclpy.executors import SingleThreadedExecutor
+from rclpy.node import Node
+from rclpy.qos import QoSDurabilityPolicy, QoSProfile, QoSReliabilityPolicy
 
 # rr_sim_tests
 from rr_sim_tests.utils.wait_for_service import wait_for_service
 from rr_sim_tests.utils.wait_for_spawned_entity import wait_for_spawned_entity
 
-SERVICE_NAME_SPAWN_ENTITY = 'SpawnEntity'
+# UE_msgs
+from ue_msgs.msg import EntityState
+from ue_msgs.srv import SpawnEntity
 
-def spawn_robot(in_robot_model, in_robot_name, in_robot_namespace, in_robot_ref_frame, in_robot_pose, in_service_namespace='',
-                in_robot_tags=None, in_robot_json='', in_timeout=5.0):
-    assert(len(in_robot_model) > 0)
-    assert(len(in_robot_name) > 0)
-    node = rclpy.create_node(f'spawn_{in_robot_name}')
-    cli = wait_for_service(node, SpawnEntity, in_service_namespace +'/' + SERVICE_NAME_SPAWN_ENTITY)
+SERVICE_NAME_SPAWN_ENTITY = "SpawnEntity"
+
+
+def spawn_robot(
+    in_robot_model,
+    in_robot_name,
+    in_robot_namespace,
+    in_robot_ref_frame,
+    in_robot_pose,
+    in_service_namespace="",
+    in_robot_tags=None,
+    in_robot_json="",
+    in_timeout=5.0,
+):
+    assert len(in_robot_model) > 0
+    assert len(in_robot_name) > 0
+    node = rclpy.create_node(f"spawn_{in_robot_name}")
+    cli = wait_for_service(
+        node, SpawnEntity, in_service_namespace + "/" + SERVICE_NAME_SPAWN_ENTITY
+    )
     if not cli.service_is_ready():
         return False
 
@@ -48,7 +59,7 @@ def spawn_robot(in_robot_model, in_robot_name, in_robot_namespace, in_robot_ref_
     future = cli.call_async(req)
     try:
         start = time.time()
-        while (time.time() - start < in_timeout):
+        while (time.time() - start) < in_timeout:
             rclpy.spin_once(node)
             if future.done():
                 break
@@ -57,17 +68,31 @@ def spawn_robot(in_robot_model, in_robot_name, in_robot_namespace, in_robot_ref_
         node.destroy_node()
     return True
 
-TOPIC_NAME_CMD_VEL = 'cmd_vel'
+
+TOPIC_NAME_CMD_VEL = "cmd_vel"
 PUBLISHING_NUM = 1
 PUBLISHING_FREQ = 10
 
+
 class CmdVelPublisher(Node):
-    def __init__(self, in_robot_namespace:str = '', publishing_num = PUBLISHING_NUM, publishing_freq = PUBLISHING_FREQ, topic_name = TOPIC_NAME_CMD_VEL, in_robot_twist:Twist = Twist()):
-        super().__init__(node_name='cmd_vel_publisher', namespace=in_robot_namespace)
+    def __init__(
+        self,
+        in_robot_namespace: str = "",
+        publishing_num=PUBLISHING_NUM,
+        publishing_freq=PUBLISHING_FREQ,
+        topic_name=TOPIC_NAME_CMD_VEL,
+        in_robot_twist: Twist = Twist(),
+    ):
+        super().__init__(node_name="cmd_vel_publisher", namespace=in_robot_namespace)
         self._cmd_vel_topic = topic_name
-        latching_qos = QoSProfile(depth=1,
-            durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL)
-        self._twist_pub = self.create_publisher(Twist, self._cmd_vel_topic, qos_profile=latching_qos)
+        latching_qos = QoSProfile(
+            depth=1,
+            durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
+            reliability=QoSReliabilityPolicy.RELIABLE,
+        )
+        self._twist_pub = self.create_publisher(
+            Twist, self._cmd_vel_topic, qos_profile=latching_qos
+        )
         self._twist = in_robot_twist
 
         if publishing_num > 0 and publishing_freq > 0:
@@ -78,47 +103,64 @@ class CmdVelPublisher(Node):
             self._executor = SingleThreadedExecutor(context=self.context)
             self._executor.add_node(self)
 
-            self._timer = self.create_timer(1.0/publishing_freq, self.twist_robot)
+            self._timer = self.create_timer(1.0 / publishing_freq, self.twist_robot)
 
             while rclpy.ok() and not self._is_pub_finished:
-                self._executor.spin_once(timeout_sec = 0)
-            print(f'Finished publishing Twist to {self.get_full_topic_name()}')
+                self._executor.spin_once(timeout_sec=0)
+            print(f"Finished publishing Twist to {self.get_full_topic_name()}")
 
     def get_full_topic_name(self):
-        return f'{self.get_namespace()}/{self._cmd_vel_topic}'
+        return f"{self.get_namespace()}/{self._cmd_vel_topic}"
 
     def __enter__(self):
         return self
 
     def __exit__(self, exep_type, exep_value, trace):
         if exep_type is not None:
-            raise Exception('Exception occured, value: ', exep_value)
-        self._executor.shutdown(timeout_sec = 0.5)
+            raise Exception("Exception occured, value: ", exep_value)
+        self._executor.shutdown(timeout_sec=0.5)
         self.destroy_node()
-        
+
     async def twist_robot(self):
-        if (self._twist_pub_count < self._publishing_num or self._publishing_num <= 0):
-            self.get_logger().info(f'Publishing to {self.get_full_topic_name()}: {self._twist}')
-            self._twist_pub.publish(self._twist)
+        if self._twist_pub_count < self._publishing_num or self._publishing_num <= 0:
+            self.get_logger().info(
+                f"Publishing to {self.get_full_topic_name()}: {self._twist}"
+            )
+            self.pub(self._twist)
             self._twist_pub_count += 1
         else:
             self._is_pub_finished = True
             self._timer.cancel()
         await asyncio.sleep(0)
-    
-    def pub(self, twist = None):
+
+    def pub(self, twist=None):
         if twist is None:
             twist = self._twist
         self._twist_pub.publish(twist)
 
-    def wait_for_robot_twisted(self, in_robot_name, in_robot_prev_pose, in_timeout=5.0):
-        assert self._is_pub_finished, f'Twist has not been published to {self.get_full_topic_name()}'
-        
+    def wait_for_robot_twisted(
+        self,
+        in_robot_name,
+        in_robot_prev_pose,
+        in_timeout=5.0,
+        publish_twist_on_fail=True,
+    ):
+        assert (
+            self._is_pub_finished
+        ), f"Twist has not been published to {self.get_full_topic_name()}"
+
         start_time = time.time()
-        while ((time.time() - start_time) < in_timeout):
+        while (time.time() - start_time) < in_timeout:
             time.sleep(1.0)
-            is_robot_found, robot_pose = wait_for_spawned_entity(in_robot_name, in_timeout=5.0)
-            assert is_robot_found, f'wait_for_robot_twisted(): {in_robot_name} unavailable!'
-            if (robot_pose != in_robot_prev_pose):
+            is_robot_found, robot_pose = wait_for_spawned_entity(
+                in_robot_name, in_timeout=5.0
+            )
+            assert (
+                is_robot_found
+            ), f"wait_for_robot_twisted(): {in_robot_name} unavailable!"
+            if robot_pose != in_robot_prev_pose:
                 return True
+            elif publish_twist_on_fail:
+                self.pub()
+
         return False
