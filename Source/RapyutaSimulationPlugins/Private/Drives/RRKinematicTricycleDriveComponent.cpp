c6ed1bee81b99cf75dcf2df73c6520f8cb0a0d8b
duc <ducanh.than@rapyuta-robotics.com>
2022-12-01 18:03:21 +0900

    Add RRKinematicTricycleDriveComponent

--- Source/RapyutaSimulationPlugins/Private/Drives/RRKinematicTricycleDriveComponent.cpp
+++ Source/RapyutaSimulationPlugins/Private/Drives/RRKinematicTricycleDriveComponent.cpp
@@ -1,11 +1,11 @@
 // Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
 
-#include "Drives/KinematicTricycleDriveComponent.h"
+#include "Drives/RRKinematicTricycleDriveComponent.h"
 
-DEFINE_LOG_CATEGORY(LogKinematicTricycleDriveComponent);
-void UKinematicTricycleDriveComponent::TickComponent(float InDeltaTime,
-                                                     enum ELevelTick TickType,
-                                                     FActorComponentTickFunction* ThisTickFunction)
+DEFINE_LOG_CATEGORY(LogRRKinematicTricycleDriveComponent);
+void URRKinematicTricycleDriveComponent::TickComponent(float InDeltaTime,
+                                                       enum ELevelTick TickType,
+                                                       FActorComponentTickFunction* ThisTickFunction)
 {
     if (!ShouldSkipUpdate(InDeltaTime))
     {
@@ -16,42 +16,42 @@ void UKinematicTricycleDriveComponent::TickComponent(float InDeltaTime,
             float steerAngle = FMath::DegreesToRadians(SteeringJoint->Orientation.Roll);
             float wheelAngle = FMath::DegreesToRadians(DriveJoint->Orientation.Roll);
 
-            float driveDiff = (wheelAngle - PrevWheeAngle) * WheelRadius;
+            float driveDiff = (wheelAngle - PrevWheeAngleRad) * WheelRadius;
             const FVector linearDiff = FVector(driveDiff * FMath::Cos(steerAngle), 0, 0);
             const FQuat rotationDiff = FQuat(FVector(0, 0, 1), driveDiff * FMath::Sin(steerAngle) / WheelBase);
 
             FHitResult hit;
             UpdatedComponent->AddLocalTransform(FTransform(rotationDiff, linearDiff), true, &hit);
 
-            PrevWheeAngle = wheelAngle;
+            PrevWheeAngleRad = wheelAngle;
         }
         else
         {
             UE_LOG(LogKinematicTricycleDriveComponent,
                    Warning,
-                   TEXT("[%s] [UKinematicTricycleDriveComponent] Steering and/or drive joints are not speciied"),
+                   TEXT("[%s] [URRKinematicTricycleDriveComponent] Steering and/or drive joints are not speciied"),
                    *GetName());
         }
     }
 }
 
-void UKinematicTricycleDriveComponent::SetDriveJoints(URRJointComponent* InSteeringJoint, URRJointComponent* InDriveJoint)
+void URRKinematicTricycleDriveComponent::SetDriveJoints(URRJointComponent* InSteeringJoint, URRJointComponent* InDriveJoint)
 {
     SteeringJoint = InSteeringJoint;
     DriveJoint = InDriveJoint;
 }
 
-void UKinematicTricycleDriveComponent::BeginPlay()
+void URRKinematicTricycleDriveComponent::BeginPlay()
 {
     if (SteeringJoint != nullptr && DriveJoint != nullptr)
     {
-        PrevWheeAngle = FMath::DegreesToRadians(DriveJoint->Orientation.Roll);
+        PrevWheeAngleRad = FMath::DegreesToRadians(DriveJoint->Orientation.Roll);
     }
     else
     {
-        UE_LOG(LogKinematicTricycleDriveComponent,
+        UE_LOG(LogRRKinematicTricycleDriveComponent,
                Warning,
-               TEXT("[%s] [UKinematicTricycleDriveComponent] Steering and/or drive joints are not speciied"),
+               TEXT("[%s] [URRKinematicTricycleDriveComponent] Steering and/or drive joints are not speciied"),
                *GetName());
     }
     Super::BeginPlay();
