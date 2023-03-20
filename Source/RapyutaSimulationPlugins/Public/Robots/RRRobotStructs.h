78a70f3e63fcff25498c085e894a1ca92670c0d2
duc <ducanh.than@rapyuta-robotics.com>
2023-03-20 19:59:05 +0900

    RRAssetUtils add CreateUClass() FRRRobotWheelProperty tune wheel

--- Source/RapyutaSimulationPlugins/Public/Robots/RRRobotStructs.h
+++ Source/RapyutaSimulationPlugins/Public/Robots/RRRobotStructs.h
@@ -901,7 +901,7 @@ public:
         FRRRobotWheelProperty wheelProp(MoveTemp(InWheelName));
         // [Radius/Width]
         wheelProp.WheelRadius = bFrontWheel ? 13.f : (wheelProp.WheelName.Contains(TEXT("caster")) ? 9.f : 16.f);
-        wheelProp.WheelWidth = bFrontWheel ? 10.f : 20.f;
+        wheelProp.WheelWidth = bFrontWheel ? 10.f : (wheelProp.WheelName.Contains(TEXT("caster")) ? 25.f : 14.f);
 
         // [AxleType]
         wheelProp.AxleType = bFrontWheel ? EAxleType::Front : EAxleType::Rear;
@@ -909,9 +909,10 @@ public:
         // [Sundries]
         wheelProp.CorneringStiffness = bFrontWheel ? 1000.f : 10000.f;
         wheelProp.FrictionForceMultiplier = bFrontWheel ? 2.f : 20.f;
-        //wheelProp.FrictionForceMultiplier = bFrontWheel ? 100.f : 10000.f;
-        //wheelProp.SideSlipModifier = 100.f;
-        //wheelProp.WheelLoadRatio = 0.5f;
+#if RAPYUTA_SIM_DEBUG
+        wheelProp.SideSlipModifier = 1.f;
+        wheelProp.WheelLoadRatio = 0.5f;
+#endif
 
         wheelProp.bAffectedByHandbrake = !bFrontWheel;
         wheelProp.bAffectedByEngine = !bFrontWheel;
@@ -922,10 +923,11 @@ public:
         wheelProp.SuspensionMaxRaise = 1.f;
         wheelProp.SuspensionMaxDrop = 1.f;
         wheelProp.SuspensionDampingRatio = bFrontWheel ? 1.f : 0.5f;
+        wheelProp.SuspensionSmoothing = 10.f;
 
         // [Spring]
-        wheelProp.SpringRate = 100.f;
-        wheelProp.SpringPreload = 10.f;
+        wheelProp.SpringRate = bFrontWheel ? 100.f : 250.f;
+        wheelProp.SpringPreload = bFrontWheel ? 10.f : 50.f;
 
         return wheelProp;
     }
