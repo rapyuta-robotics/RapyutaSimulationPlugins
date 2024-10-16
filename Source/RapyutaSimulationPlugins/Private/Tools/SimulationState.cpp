c406ee5865f95a2ea100343f2207fcdf594ad896
Larry Ng <larry.ng@rapyuta-robotics.com>
2024-10-16 17:27:57 +0900

    fix entity state

--- Source/RapyutaSimulationPlugins/Private/Tools/SimulationState.cpp
+++ Source/RapyutaSimulationPlugins/Private/Tools/SimulationState.cpp
@@ -212,7 +212,7 @@ bool ASimulationState::ServerCheckSetEntityStateRequest(const FROSSetEntityState
         PrevSetEntityStateRequest.State.Pose.Position == InRequest.State.Pose.Position &&
         PrevSetEntityStateRequest.State.Pose.Orientation == InRequest.State.Pose.Orientation)
     {
-        return false;
+        return true;
     }
     else
     {
