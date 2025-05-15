91bd4102ae25f14ae7cc2cd4a1f4061ede9a8492
yuokamoto <yuokamoto1988@gmail.com>
2025-05-15 21:40:28 +0900

    make sensor component msg qos configurable from robot (#366)
    * make sensor component msg qos configurable from robot
    
    * Update RRROS2OverlapSensorComponent.h
    
    update documentation

--- Source/RapyutaSimulationPlugins/Private/Sensors/RRPoseSensorManager.cpp
+++ Source/RapyutaSimulationPlugins/Private/Sensors/RRPoseSensorManager.cpp
@@ -31,11 +31,10 @@ void URRPoseSensorManager::OnComponentCreated()
 
 void URRPoseSensorManager::InitalizeWithROS2(UROS2NodeComponent* InROS2Node,
                                              const FString& InPublisherName,
-                                             const FString& InTopicName,
-                                             const UROS2QoS InQoS)
+                                             const FString& InTopicName)
 {
     UE_LOG_WITH_INFO_NAMED(LogRapyutaCore, Warning, TEXT("%s"), *ReferenceTag);
-    Super::InitalizeWithROS2(InROS2Node, InPublisherName, InTopicName, InQoS);
+    Super::InitalizeWithROS2(InROS2Node, InPublisherName, InTopicName);
     MapOriginPoseSensor->InitalizeWithROS2(InROS2Node);
     ServerSimState = CastChecked<ASimulationState>(UGameplayStatics::GetActorOfClass(GetWorld(), ASimulationState::StaticClass()));
 }
